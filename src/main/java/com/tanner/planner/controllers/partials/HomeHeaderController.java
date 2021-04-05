d5981b9d3daa7832072237e082465f339ee99344
Erhmee1 <47233977+erhmee1@users.noreply.github.com>
2021-04-05 17:17:50 +0800

    "home page layout with base"

---
+++ src/main/java/com/tanner/planner/controllers/partials/HomeHeaderController.java
@@ -0,0 +1,14 @@
+package com.tanner.planner.controllers.partials;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeHeaderController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/com/tanner/planner/controllers/partials/SidebarController.java
@@ -0,0 +1,15 @@
+package com.tanner.planner.controllers.partials;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SidebarController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
