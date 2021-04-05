f7c064334893cff426c57f2d0ef78522ca18c8f0
Erhmee1 <47233977+Erhmee1@users.noreply.github.com>
2021-04-05 13:23:12 +0800

    designed panel_layout structure

--- src/main/java/com/tanner/planner/Main.java
+++ src/main/java/com/tanner/planner/Main.java
@@ -1,18 +1,22 @@
 package com.tanner.planner;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
-        VBox vbox = new VBox();
-        Scene scene = new Scene(vbox);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/controllers/home_layout.fxml"));
+        primaryStage.setTitle("Tanner");
+        Scene scene = new Scene(root, 1440, 1024);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/tanner/planner/controllers/PanelController.java
+++ src/main/java/com/tanner/planner/controllers/PanelController.java
@@ -1,4 +1,36 @@
 package com.tanner.planner.controllers;
 
-public class PanelController {
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.shape.SVGPath;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PanelController implements Initializable {
+
+    @FXML
+    private Button btnHamburgerMenu;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ImageView menu = new ImageView("/images/menu.png");
+        menu.setFitHeight(20);
+        menu.setFitWidth(20);
+        //menu.setPreserveRatio(true);
+        //btnHamburgerMenu.setGraphic(menu);
+
+        Image image = new Image("/images/menu.png", btnHamburgerMenu.getWidth(), btnHamburgerMenu.getHeight(), false, true, true);
+        BackgroundImage bImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(btnHamburgerMenu.getWidth(), btnHamburgerMenu.getHeight(), true, true, true, false));
+
+        Background backGround = new Background(bImage);
+        btnHamburgerMenu.setBackground(backGround);
+
+    }
 }
